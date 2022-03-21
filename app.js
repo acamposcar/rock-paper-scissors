2cfafdbc13a6f97e7ecb006424bd362e8d687332
Alejandro Campos <acamposcar@gmail.com>
2022-03-21 21:49:21 +0100

    Prettify

--- app.js
+++ app.js
@@ -1,102 +1,94 @@
 // @ts-check
 
-function computerPlay(){
-    const plays = ['Rock', 'Paper', 'Scissors']
-    return plays[Math.floor(Math.random() * plays.length)].toLowerCase();
+function computerPlay() {
+  const plays = ["Rock", "Paper", "Scissors"];
+  return plays[Math.floor(Math.random() * plays.length)].toLowerCase();
 }
 
-function tie(){
-    document.querySelector('#player-icon').classList.remove('bg-purple-800')
-    document.querySelector('#computer-icon').classList.remove('bg-purple-800')
-    document.querySelector('#result').textContent = "It's a tie!";
+function tie() {
+  document.querySelector("#player-icon").classList.remove("bg-purple-800");
+  document.querySelector("#computer-icon").classList.remove("bg-purple-800");
+  document.querySelector("#result").textContent = "It's a tie!";
 }
 
-function lose(playerSelection, computerSelection){
-    const prevScore = parseInt(document.querySelector('#computer-score').textContent)
-    document.querySelector('#computer-score').textContent = (prevScore + 1).toString()
-    document.querySelector('#player-icon').classList.remove('bg-purple-800')
-    document.querySelector('#computer-icon').classList.add('bg-purple-800')
-    document.querySelector('#result').textContent = 'You lose!';
+function lose() {
+  const prevScore = parseInt(document.querySelector("#computer-score").textContent);
+  document.querySelector("#computer-score").textContent = (prevScore + 1).toString();
+  document.querySelector("#player-icon").classList.remove("bg-purple-800");
+  document.querySelector("#computer-icon").classList.add("bg-purple-800");
+  document.querySelector("#result").textContent = "You lose!";
 }
 
-function win(playerSelection, computerSelection){
-    const prevScore = parseInt(document.querySelector('#player-score').textContent)
-    document.querySelector('#player-score').textContent = (prevScore + 1).toString()
-    document.querySelector('#player-icon').classList.add('bg-purple-800')
-    document.querySelector('#computer-icon').classList.remove('bg-purple-800')
-    document.querySelector('#result').textContent = 'You win!';
+function win() {
+  const prevScore = parseInt(document.querySelector("#player-score").textContent);
+  document.querySelector("#player-score").textContent = (prevScore + 1).toString();
+  document.querySelector("#player-icon").classList.add("bg-purple-800");
+  document.querySelector("#computer-icon").classList.remove("bg-purple-800");
+  document.querySelector("#result").textContent = "You win!";
 }
 
-function updateEmoji(playerSelection, computerSelection){
-    const playerEmoji = document.querySelector('#player-icon')
-    if (playerSelection === 'rock') {
-        playerEmoji.textContent = "✊";
-    } 
-    else if (playerSelection === 'paper') {
-        playerEmoji.textContent = "✋";
-    } 
-    else {
-        playerEmoji.textContent = "✌";
-    }
+function updateEmoji(playerSelection, computerSelection) {
+  const playerEmoji = document.querySelector("#player-icon");
+  if (playerSelection === "rock") {
+    playerEmoji.textContent = "✊";
+  } else if (playerSelection === "paper") {
+    playerEmoji.textContent = "✋";
+  } else {
+    playerEmoji.textContent = "✌";
+  }
 
-    const computerEmoji = document.querySelector('#computer-icon')
-    if (computerSelection === 'rock') {
-        computerEmoji.textContent = "✊";
-    } 
-    else if (computerSelection === 'paper') {
-        computerEmoji.textContent = "✋";
-    } 
-    else {
-        computerEmoji.textContent = "✌";
-    }
+  const computerEmoji = document.querySelector("#computer-icon");
+  if (computerSelection === "rock") {
+    computerEmoji.textContent = "✊";
+  } else if (computerSelection === "paper") {
+    computerEmoji.textContent = "✋";
+  } else {
+    computerEmoji.textContent = "✌";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    round += 1;
-    document.querySelector('#round').textContent = round.toString()
-    updateEmoji(playerSelection, computerSelection);
+  round += 1;
+  document.querySelector("#round").textContent = round.toString();
+  updateEmoji(playerSelection, computerSelection);
 
-    if (playerSelection === computerSelection) {
-        tie()
-    } else if (playerSelection === 'rock'){
-
-        if (computerSelection === 'scissors') {
-            win(playerSelection, computerSelection)
-        }
-
-        if (computerSelection === 'paper'){
-            lose(playerSelection, computerSelection)
-        }
-        
-    } else if (playerSelection === 'paper'){
-        if (computerSelection === 'scissors') {
-            win(playerSelection, computerSelection)
-        }
-
-        if (computerSelection === 'rock'){
-            lose(playerSelection, computerSelection)
-        }
-    } else if (playerSelection === 'scissors'){
-        if (computerSelection === 'paper') {
-            lose(playerSelection, computerSelection)
-        }
-
-        if (computerSelection === 'rock'){
-            win(playerSelection, computerSelection)
-        }
+  if (playerSelection === computerSelection) {
+    tie();
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      win();
     }
-    
 
+    if (computerSelection === "paper") {
+      lose();
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      win();
+    }
+
+    if (computerSelection === "rock") {
+      lose();
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      lose();
+    }
+
+    if (computerSelection === "rock") {
+      win();
+    }
+  }
 }
 
-document.querySelector('#player-rock').addEventListener('click', () => {
-  playRound('rock', computerPlay())
+document.querySelector("#player-rock").addEventListener("click", () => {
+  playRound("rock", computerPlay());
 });
-document.querySelector('#player-paper').addEventListener('click', () => {
-  playRound('paper', computerPlay())
+document.querySelector("#player-paper").addEventListener("click", () => {
+  playRound("paper", computerPlay());
 });
-document.querySelector('#player-scissors').addEventListener('click', () => {
-  playRound('scissors', computerPlay())
+document.querySelector("#player-scissors").addEventListener("click", () => {
+  playRound("scissors", computerPlay());
 });
 
 let round = 0;
