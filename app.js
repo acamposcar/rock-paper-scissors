af9fe3e4e5f5d9d2ffe69c0985676cfae8fa1645
Alejandro Campos <acamposcar@gmail.com>
2022-03-21 21:45:18 +0100

    Add UI

--- app.js
+++ app.js
@@ -5,56 +5,98 @@ function computerPlay(){
     return plays[Math.floor(Math.random() * plays.length)].toLowerCase();
 }
 
-function playerPlay(){
-    return prompt('What is your selection?').toLowerCase();
+function tie(){
+    document.querySelector('#player-icon').classList.remove('bg-purple-800')
+    document.querySelector('#computer-icon').classList.remove('bg-purple-800')
+    document.querySelector('#result').textContent = "It's a tie!";
 }
 
 function lose(playerSelection, computerSelection){
-    return `You win. ${playerSelection} beats ${computerSelection} `;
+    const prevScore = parseInt(document.querySelector('#computer-score').textContent)
+    document.querySelector('#computer-score').textContent = (prevScore + 1).toString()
+    document.querySelector('#player-icon').classList.remove('bg-purple-800')
+    document.querySelector('#computer-icon').classList.add('bg-purple-800')
+    document.querySelector('#result').textContent = 'You lose!';
 }
 
 function win(playerSelection, computerSelection){
-    return `You win. ${playerSelection} beats ${computerSelection} `;
+    const prevScore = parseInt(document.querySelector('#player-score').textContent)
+    document.querySelector('#player-score').textContent = (prevScore + 1).toString()
+    document.querySelector('#player-icon').classList.add('bg-purple-800')
+    document.querySelector('#computer-icon').classList.remove('bg-purple-800')
+    document.querySelector('#result').textContent = 'You win!';
+}
+
+function updateEmoji(playerSelection, computerSelection){
+    const playerEmoji = document.querySelector('#player-icon')
+    if (playerSelection === 'rock') {
+        playerEmoji.textContent = "✊";
+    } 
+    else if (playerSelection === 'paper') {
+        playerEmoji.textContent = "✋";
+    } 
+    else {
+        playerEmoji.textContent = "✌";
+    }
+
+    const computerEmoji = document.querySelector('#computer-icon')
+    if (computerSelection === 'rock') {
+        computerEmoji.textContent = "✊";
+    } 
+    else if (computerSelection === 'paper') {
+        computerEmoji.textContent = "✋";
+    } 
+    else {
+        computerEmoji.textContent = "✌";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    round += 1;
+    document.querySelector('#round').textContent = round.toString()
+    updateEmoji(playerSelection, computerSelection);
+
     if (playerSelection === computerSelection) {
-        return 'Draw';
+        tie()
     } else if (playerSelection === 'rock'){
 
         if (computerSelection === 'scissors') {
-            return win(playerSelection, computerSelection)
+            win(playerSelection, computerSelection)
         }
 
         if (computerSelection === 'paper'){
-            return lose(playerSelection, computerSelection)
+            lose(playerSelection, computerSelection)
         }
         
     } else if (playerSelection === 'paper'){
         if (computerSelection === 'scissors') {
-            return win(playerSelection, computerSelection)
+            win(playerSelection, computerSelection)
         }
 
         if (computerSelection === 'rock'){
-            return lose(playerSelection, computerSelection)
+            lose(playerSelection, computerSelection)
         }
     } else if (playerSelection === 'scissors'){
         if (computerSelection === 'paper') {
-            return lose(playerSelection, computerSelection)
+            lose(playerSelection, computerSelection)
         }
 
         if (computerSelection === 'rock'){
-            return win(playerSelection, computerSelection)
+            win(playerSelection, computerSelection)
         }
-    } else {
-        return 'Invalid play'
-
     }
+    
+
 }
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerPlay(), computerPlay()));
-    }
-}
+document.querySelector('#player-rock').addEventListener('click', () => {
+  playRound('rock', computerPlay())
+});
+document.querySelector('#player-paper').addEventListener('click', () => {
+  playRound('paper', computerPlay())
+});
+document.querySelector('#player-scissors').addEventListener('click', () => {
+  playRound('scissors', computerPlay())
+});
+
+let round = 0;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['index.html', 'app.js'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
+  },  
+  plugins: [],
+}
